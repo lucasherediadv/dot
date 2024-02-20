86acee55af75ebe77b3a32877a114138ca27ded8
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-02-20 16:38:26 -0300

    Key mappings for buffer navigation

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,15 +1,19 @@
 local map = vim.keymap.set
 
--- Move to window using the <ctrl> hjkl keys
+-- Better movement in splits
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true})
 map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true})
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true})
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true})
 
--- Move lines with <Alt> jk keys
+-- Move lines
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
 map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
 
--- Clear search with <esc>
+-- Clear highlight search
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- Move between buffers
+map("n", "<space>]", vim.cmd.bnext)
+map("n", "<space>[", vim.cmd.bprev)
 
