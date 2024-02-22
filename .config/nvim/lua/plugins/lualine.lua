6057f43c3578a84173fc69fdbb03a48a55836a9a
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-02-22 08:31:21 -0300

    Adding plugin description

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,15 @@
+-- Neovim statusline written in Lua
+
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    options = {
-      globalstatus = true, -- Don't break the bar in splits
-    },
+    options = { globalstatus = true }, -- Don't break the bar in splits
     sections = {
-      lualine_c = {
-        {
-          "filename", file_status = true, path = 1, -- Show relative path to file
-        },
-      },
-
+      -- Show relative path to file
+      lualine_c = { { "filename", file_status = true, path = 1 } },
+      -- Notify in status line when plugins have updates
+      lualine_x = { { require("lazy.status").updates, cond = require("lazy.status").has_updates, } }
     },
   },
 }
