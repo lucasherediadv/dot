bfff677847143e8ba741948d3ce2972327c84336
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-02-27 17:41:40 -0300

    Removing icons - Change separators - Add relative path to file

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,8 +4,17 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    theme = "vscode", -- Enable colorscheme in lualine
-    options = { globalstatus = true }, -- Don't break the bar in splits
+    options = {
+      theme = "auto",
+      globalstatus = true, -- Don't break the bar in splits
+      icons_enabled = false, -- Disable icons
+      component_separators = '|', -- Change default separator
+      section_separators = '', -- Change default separator
+    },
+    sections = {
+      -- Show relative path to file
+      lualine_c = { { "filename", file_status = true, path = 1 } },
+    },
   },
 }
 
