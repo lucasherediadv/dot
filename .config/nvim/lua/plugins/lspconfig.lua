cc25e4e4cfba37188e32a2c31039f2564179e37b
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-02-27 22:14:10 -0300

    Adding useful key mappings

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -7,9 +7,27 @@ return {
     'folke/neodev.nvim',
   },
   config = function()
+
+    -- Keymaps to navigate through diagnostics
+    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float) -- Open diagnostic in float
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev) -- Got to previous diagnostic
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next) -- Go to next diagnostic
+
+    -- Function to display hover information about the symbol
+    -- under the cursor in a floating window
+    -- Calling the function twice will jump into the floating window
+    local my_on_attach = function (_, bufnr)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr})
+    end
+
     require("neodev").setup()
-    require('lspconfig').lua_ls.setup({})
-    require('lspconfig').pyright.setup({})
+    require('lspconfig').lua_ls.setup({
+      on_attach = my_on_attach,
+    })
+
+    require('lspconfig').pyright.setup({
+      on_attach = my_on_attach,
+    })
   end,
 }
 
