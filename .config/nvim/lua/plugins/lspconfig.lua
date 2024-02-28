494af2ff2bbbcdac95db8826e4af40a1a125d2c3
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-02-28 20:31:50 -0300

    Adding settings for Pyright

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -20,13 +20,44 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr})
     end
 
+    -- Allows lua to work with neovim
     require("neodev").setup()
+
+    -- Lua LSP setup and config
     require('lspconfig').lua_ls.setup({
       on_attach = my_on_attach,
+      filetypes = {"lua"}
     })
 
-    require('lspconfig').pyright.setup({
-      on_attach = my_on_attach,
+    -- Pyright config
+    require("lspconfig").pyright.setup({
+      on_attach = my_on_attach(),
+      filetypes = {"python"},
+
+      settings = {
+        python = {
+          analysis = {
+            -- Determines whether pyright analyzes all files in the workspace
+            diagnosticMode = "openFilesOnly",
+            -- Determines the default type-checking level used by pyright
+            typeCheckingMode = "strict",
+            -- Determines whether pyright offers auto-import completions
+            autoImportCompletions = true,
+            -- Determines whether pyright automatically adds common
+            -- search paths like "src" if there are no execution
+            -- environments defined in the config file
+            autoSearchPaths = true,
+            -- Determines whether pyright reads, parses and analyzes library
+            -- code to extract type information in the absence of type
+            -- stubs files
+            useLibraryCodeForTypes = true,
+          }
+        }
+      },
+      keys = {
+        -- Organize imports in python files
+        vim.keymap.set("n", "<leader>oi", ": PyrightOrganizeImports<CR>", { desc = "Organize Imports in python files" })
+      },
     })
   end,
 }
