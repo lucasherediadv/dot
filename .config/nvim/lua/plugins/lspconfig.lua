1d771642e1d610e83825c3f16a29a12b35fae09e
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-02 16:22:04 -0300

    Setup marksman LSP - Add new key mappings

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -7,33 +7,62 @@ return {
     'folke/neodev.nvim',
   },
   config = function()
-
-    -- Keymaps to navigate through diagnostics
+    -- Global mappings.
+    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
     vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float) -- Open diagnostic in float
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev) -- Got to previous diagnostic
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next) -- Go to next diagnostic
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)         -- Got to previous diagnostic
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)         -- Go to next diagnostic
+    -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-    -- Function to display hover information about the symbol
-    -- under the cursor in a floating window
-    -- Calling the function twice will jump into the floating window
-    local my_on_attach = function (_, bufnr)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr})
-    end
+    -- Use LspAttach autocommand to only map the following keys
+    -- after the language server attaches to the current buffer
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Allows lua to work with neovim
-    require("neodev").setup()
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        -- vim.keymap.set('n', '<space>wl', function()
+        -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        -- end, opts)
+        -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>fr', function()
+          vim.lsp.buf.format { async = true }
+        end, opts)
 
-    -- Lua LSP setup and config
-    require('lspconfig').lua_ls.setup({
-      on_attach = my_on_attach,
-      filetypes = {"lua"}
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+          vim.lsp.handlers.hover,
+          { border = 'rounded' }
+        )
+
+        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+          vim.lsp.handlers.signature_help,
+          { border = 'rounded' }
+        )
+        vim.diagnostic.config({
+          float = {
+            border = 'rounded',
+          },
+        })
+      end,
     })
 
-    -- Pyright config
+    require("neodev").setup()
+    require('lspconfig').lua_ls.setup({})
     require("lspconfig").pyright.setup({
-      on_attach = my_on_attach(),
-      filetypes = {"python"},
-
       settings = {
         python = {
           analysis = {
@@ -58,7 +87,7 @@ return {
         -- Organize imports in python files
         vim.keymap.set("n", "<leader>oi", ": PyrightOrganizeImports<CR>", { desc = "Organize Imports in python files" })
       },
+      require "lspconfig".marksman.setup {}
     })
   end,
 }
-
