52d79bedbfb46068c58bf909530e876d995f12c3
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-02 16:19:53 -0300

    Simplifying configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
 -- Neovim color scheme inspired by Dark theme in Visual Studio Code
 
 return {
-'Mofiqul/vscode.nvim',
-  lazy = false, -- Make sure to load this during startup
+  'Mofiqul/vscode.nvim',
+  lazy = false,    -- Make sure to load this during startup
   priority = 1000, -- Make sure to load this before all the other start plugins
   config = function()
-    require('vscode').setup({
-      italic_comments = true, -- Set italic format to comments
-      underline_links = true, -- Underline `@markup.link.*` variants
-     })
-     vim.cmd([[colorscheme vscode]])
+    vim.cmd([[colorscheme vscode]])
   end,
 }
-
