ddc52d3e9f6b0217efdb58015b0d3defb046f187
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-02 16:15:36 -0300

    Removing unused key mappings - Adding descriptions

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-local map = vim.keymap.set
+local map = vim.keymap.set -- Set new keymap
 
 -- Better movement in splits with <Ctrl>hjkl
 map("n", "<C-h>", "<C-w>h", { remap = true})
@@ -6,23 +6,15 @@ map("n", "<C-j>", "<C-w>j", { remap = true})
 map("n", "<C-k>", "<C-w>k", { remap = true})
 map("n", "<C-l>", "<C-w>l", { remap = true})
 
--- Move lines up and down with <Atl>jk
-map("n", "<A-j>", "<cmd>m .+1<cr>==")
-map("n", "<A-k>", "<cmd>m .-2<cr>==")
-
 -- Clear highlight search with <esc>
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>")
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear highlight search" })
 
 -- Move between buffers with <space>[]
-map("n", "<space>]", vim.cmd.bnext)
-map("n", "<space>[", vim.cmd.bprev)
+map("n", "<space>]", vim.cmd.bnext, { desc = "Go to next buffer" })
+map("n", "<space>[", vim.cmd.bprev, { desc = "Go to previous buffer "})
 
  -- Close current buffer
 map("n", "<leader>bd", ":bd!<CR>")
-
--- After tab in/out re-select the same
-vim.keymap.set('v', '>', '>gv')
-vim.keymap.set('v', '<', '<gv')
 
 -- Goes to the next/prev result on the search and put the cursor in
 -- the middle
