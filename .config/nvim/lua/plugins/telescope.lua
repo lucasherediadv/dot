43c60d6fb7054ee9918a26a62572e341b78d35de
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-03 10:06:32 -0300

    Formatting

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,42 +4,42 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-file-browser.nvim',
-    {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   opts = {
     extensions = {
       fzf = {
-        fuzzy = true, -- false will only do exact matching
+        fuzzy = true,                   -- false will only do exact matching
         override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or 'ignore_case or 'respect_case'
-                                  -- the default case_mode is 'smart_case'
+        override_file_sorter = true,    -- override the file sorter
+        case_mode = "smart_case",       -- or 'ignore_case or 'respect_case'
       },
     },
   },
+
   config = function(opts)
     require('telescope').setup(opts)
     require('telescope').load_extension('fzf')
   end,
 
-  -- Custom Keymaps
+ -- Custom Keymaps
   keys = {
     {
       "<leader>bb",
       function()
-        require("telescope").extensions.file_browser.file_browser({path = "%:h:p", select_buffer = true, hidden = true})
+        require("telescope").extensions.file_browser.file_browser({ path = "%:h:p", select_buffer = true, hidden = true })
       end,
       desc = "Telescope file browser"
     },
     {
       "<leader>ff",
       function()
-        require('telescope.builtin').find_files( {file_ignore_patterns = { "Dotfiles" } })
+        require('telescope.builtin').find_files({ file_ignore_patterns = { "Dotfiles" } })
       end,
       desc = "Telescope find files"
     },
     {
-    "<leader>kk",
+      "<leader>kk",
       function()
         require("telescope.builtin").keymaps()
       end,
@@ -54,4 +54,3 @@ return {
     },
   },
 }
-
