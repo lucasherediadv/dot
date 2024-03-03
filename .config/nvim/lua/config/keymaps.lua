03a04002aa1809957de663e43b381ae162295585
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-03 09:36:26 -0300

    Unused key mappings deleted

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,11 +13,7 @@ map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear highlight search
 map("n", "<space>]", vim.cmd.bnext, { desc = "Go to next buffer" })
 map("n", "<space>[", vim.cmd.bprev, { desc = "Go to previous buffer "})
 
- -- Close current buffer
-map("n", "<leader>bd", ":bd!<CR>")
-
--- Goes to the next/prev result on the search and put the cursor in
--- the middle
+-- Goes to the next/prev result on the search and put the cursor in the middle
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
