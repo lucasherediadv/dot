2e053128ae841a3accb96f713a6b6014b6bbb55d
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-09-05 17:18:49 -0300

    Remove neovim configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Mofiqul/vscode.nvim',
-  lazy = false,    -- Make sure to load this during startup
-  priority = 1000, -- Make sure to load this before all the other start plugins
-  config = function()
-    vim.cmd([[colorscheme vscode]])
-  end,
-}
-
