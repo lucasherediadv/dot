8b885ab7658731b136bc935cc24d2a255950eb8b
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2025-04-09 15:28:38 -0300

    Colored man pages with bat

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,9 @@ if status is-interactive
   set -Ux _ZO_ECHO 1
   set -Ux _ZO_RESOLVE_SYMLINKS 1
 
+  # Colored man pages with bat
+  set -Ux MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat --theme-dark=gruvbox-dark -p -lman'"
+
   # Custom cursor style on vi-mode
   set fish_cursor_default block
   set fish_cursor_insert block
